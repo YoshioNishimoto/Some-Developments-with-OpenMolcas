ce15fbf2a857a5ce0149353140f58e8007e971bf
Marjan Khamesian <marjan.khamesian@kemi.uu.se>
2020-03-19 11:37:02 +0100

    modified

--- src/oneint_util/trsph_dmm.f90
+++ src/oneint_util/trsph_dmm.f90
@@ -1,38 +1,39 @@
-ASubroutine dmm_tranform(l,Fi2, dmm)
+Subroutine dmm_tranform(l,Fi2, dmm)
+
   Implicit None
 
   Integer, intent(in) :: l
   Real, intent(in) :: Fi2
   Real*8, intent(out) :: dmm(dim,dim)
 
-  Integer :: k_min, k_max, m, mp
-  Integer :: fact1, fact2, fact3, fact4, a1, a2
-  Real :: coeff
-  Integer, parameter :: dim = 2*l +1
-
+  Integer :: k_min, k_max, k, m, mp, a1, a2
+  Real :: fact1, fact2, fact3, fact4, coeff
+  Integer :: dim
+  dim = 2*l + 1
 !====================================================================
 
-  m: Do m = -l, l
-     mp: Do mp = -l, l
+  Do m = -l, l
+     Do mp = -l, l
         fact1 = factorial(l+mp)
         fact2 = factorial(l-mp)
         fact3 = factorial(l+m)
         fact4 = factorial(l-m)
-        coeff = (-1)**(mp -m) * sqrt( fact1 * fact2 * fact3 * fact4)
+        coeff = (-1)**(mp -m) * sqrt(fact1 * fact2 * fact3 * fact4)
 
         Call k_interval(l, m, mp, k_min, k_max)
 
-        k: Do k = k_min, k_max
+        Do k = k_min, k_max
            a1 = choose(l+m, k)
            a2 = choose(l-m, l-mp-k)
 
-           dmm = dmm + (-1)**k * a1 * a2 * cos((Fi2)/2)**(2l-mp+m-2k)*         &
-                sin((Fi2/2))**(2k-m+mp)
-        End Do k
-     End Do mp
-  End Do m
+           dmm = dmm + (-1)**k * a1 * a2 * cos((Fi2)/2)**(2*l-mp+m-2*k)*  &
+                sin((Fi2/2))**(2*k-m+mp)
+        End Do
+     End Do
+  End Do
 
-!===================================================================
+!====================================================================
+Contains
 
   Function factorial (n) result (res)
 
