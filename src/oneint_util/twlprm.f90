e07fc4a8a6b19bdb92cff64bb2dabd6d8b684553
Marjan Khamesian <marjan.khamesian@kemi.uu.se>
2021-01-26 13:36:02 +0100

    more updates

--- src/oneint_util/twlprm.f90
+++ src/oneint_util/twlprm.f90
@@ -16,12 +16,12 @@ Subroutine twlprm(Zeta,P_x,P_y,Alpha,Beta,i_x,i_y,j_x,j_y,l,intg)
 !
 !     Local Arrays and integers
 !
-  Integer :: n, m, k, q, i_x, i_y, j_x, j_y, p, t
-  Integer :: a, b, v, w, c, d, g, f, l
+  Integer :: n, m, k, q, i_x, i_y, j_x, j_y
+  Integer :: p, t, a, b, v, w, c, d, g, f, l
   Complex*16 intg
   Real*8 :: Lambda, Theta, Zeta
-  Real*8 :: a1, a2, b1, b2, c1, c2, d1, d2, e1, e2, f1, f2
-  Real*8 :: g1, g2, h1, h2, k1, k2, l2, l3
+  Real*8 :: a1, a2, b1, b2, c1, c2, d1, d2, e1, e2
+  Real*8 :: g1, g2, h1, h2, k1, k2, l2, f1, f2
   Real*8 :: A_x, A_y, B_x, B_y, Alpha, Beta, P_x, P_y
   Real*8, parameter :: pi = atan(1.0)*4.d0
   Complex*16 cx, Kappa, i, Omega, l1, Gamma
