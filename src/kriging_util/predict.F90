975fdb9071cd40c5d44d3560b324503bfc5a66ad
roland <roland.lindh@kemi.uu.se>
2021-05-18 14:26:31 +0200

    Move forward to nSet/=1.

--- src/kriging_util/predict.F90
+++ src/kriging_util/predict.F90
@@ -13,14 +13,14 @@
 
 subroutine predict(gh)
 
-use kriging_mod, only: cv, full_R, gpred, hpred, Kv, m_t, nInter, ordinary, pred, Rones, sb, sigma, var, variance
+use kriging_mod, only: cv, full_R, gpred, hpred, Kv, m_t, nInter, ordinary, pred, Rones, sb, sigma, var, variance, nSet
 use stdalloc, only: mma_allocate, mma_deallocate
 use Constants, only: Zero, One
 use Definitions, only: wp, iwp
 
 implicit none
 integer(kind=iwp), intent(in) :: gh
-integer(kind=iwp) :: INFO, i, k
+integer(kind=iwp) :: INFO, i, k, iSet
 integer(kind=iwp), allocatable :: IPIV(:) ! ipiv the pivot indices that define the permutation matrix
 real(kind=wp) :: tsum
 real(kind=wp), allocatable :: B(:), A(:,:)
@@ -32,10 +32,17 @@ if (gh == 0) then ! calculate the energy and dispersion
   !A contains the factors L and U from the factorization A = P*L*U as computed by DGETRF
   call mma_allocate(A,m_t,m_t,label='A')
   call mma_allocate(IPIV,m_t,label='IPIV')
+
   ! calculations of Energy and dispersion
+
+Do iSet = 1, nSet
+
+  pred   = sb(iSet)+dot_product(cv(:,1,1),Kv(:,iSet))   ! compute the energy
+
+End Do
+
   A(:,:) = full_R(:,:)
   B(:)   = cv(:,1,1)    ! the covariance vector
-  pred   = sb(1)+dot_product(B,Kv(:,1))   ! compute the energy
 
 ! solve R x = c,  x = R^{-1} c
   call DGESV_(m_t,1,A,m_t,IPIV,B,m_t,INFO)
@@ -47,7 +54,10 @@ if (gh == 0) then ! calculate the energy and dispersion
     var = max(var+(One-dot_product(cv(:,1,1),rones))**2/tsum,Zero)
   end if
 
-  sigma = sqrt(var*variance(1))
+Do iSet = 1, nSet
+  sigma = sqrt(var*variance(iSet))
+End Do
+
   call mma_deallocate(A)
   call mma_deallocate(IPIV)
 
