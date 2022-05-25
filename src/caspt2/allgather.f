587f22ec915d32396c905f528bb573769a701dbb
Ernst D. Larsson <ernst.d.larsson@gmail.com>
2022-05-25 08:33:13 +0200

    Too many MPIInt?

--- src/caspt2/allgather.f
+++ src/caspt2/allgather.f
@@ -44,7 +44,8 @@
       integer(MPIInt) :: WorkAround
       INTEGER(MPIInt), ALLOCATABLE :: NRECV4(:),IDISP4(:)
       INTEGER(MPIInt), PARAMETER :: ONE4 = 1
-      integer(MPIInt) :: nBytes, myrank, nProcs, i
+!     integer(MPIInt) :: nBytes, myrank, nProcs, i
+      integer(iwp)    :: nBytes, myrank, nProcs, i
 
       ITYPE4 = MPI_REAL8
       NBYTES = 8 * NRECV
@@ -124,7 +125,8 @@
       integer(MPIInt) :: WorkAround
       INTEGER(MPIInt), ALLOCATABLE :: NRECV4(:),IDISP4(:)
       INTEGER(MPIInt), PARAMETER :: ONE4 = 1
-      integer(MPIInt) :: nBytes, myrank, nProcs, i
+!     integer(MPIInt) :: nBytes, myrank, nProcs, i
+      integer(iwp)    :: nBytes, myrank, nProcs, i
 
 #ifdef _I8_
         ITYPE4=MPI_INTEGER8
